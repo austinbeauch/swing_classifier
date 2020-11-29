0e17dd88999928b8c513e494d653f845f642ec7a
ocbier <ocbiermann@outlook.com>
2020-11-29 10:13:16 -0800

    Finished implementing mcsvm

--- skl_dataset.py
+++ skl_dataset.py
@@ -1,19 +1,17 @@
-import torch
 import numpy as np
+
 
 def norm(x, m, s):
     return (x - m) / s
 
-def augment(X):
-    return torch.roll(X * np.random.uniform(low=0.8, high=1.1), np.random.randint(-150,150))
 
-class SKLSwingDataset(torch.utils.data.Dataset):
-    def __init__(self, X_data, y_data, mean=None, std=None, y_mean=None, y_std=None, augment=False):
+
+class SKLSwingDataset():
+    def __init__(self, X_data, y_data, mean=None, std=None, y_mean=None, y_std=None):
         super().__init__()
         self.X_data = X_data
         self.y_data = y_data
-        self.augment = augment
-        
+                
         
         if mean is None: # assume that all the other values are None as well, this is the training set and we need to compute the mean/std
             self.mean = X_data.mean(axis=0)
@@ -30,12 +28,10 @@ class SKLSwingDataset(torch.utils.data.Dataset):
         return self.X_data.shape[0]
     
     def __getitem__(self, idx):
-        X = self.X_data[idx].clone()  # clone or else the normalization overwrites the actual tensor
+        X = self.X_data[idx].clone()  
         y = self.y_data[idx].clone()
         
-        if self.augment:
-            X = augment(X)
-        
+                
         X = norm(X, self.mean, self.std)
         y[-1] = norm(y[-1], self.y_dist_mean, self.y_dist_std) 
         
